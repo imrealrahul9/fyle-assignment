eb17b1501abc7e9017254990674b903028b2c2f1
imrealrahul9 <101348345+imrealrahul9@users.noreply.github.com>
2024-08-11 12:22:08 +0530

    alert and success msg updation

--- src/app/workout-form/workout-form.component.ts
+++ src/app/workout-form/workout-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { WorkoutService } from '../workout.service';
 import { Workout } from '../workout.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-workout-form',
@@ -12,7 +13,7 @@ export class WorkoutFormComponent {
   workoutForm: FormGroup;
   workoutTypes: string[] = ['Running', 'Jogging', 'Weight', 'Abs'];
 
-  constructor(private fb: FormBuilder, private workoutService: WorkoutService) {
+  constructor(private fb: FormBuilder, private workoutService: WorkoutService,private snackBar: MatSnackBar) {
     this.workoutForm = this.fb.group({
       userName: ['', Validators.required],
       workoutType: ['', Validators.required], 
@@ -32,7 +33,17 @@ export class WorkoutFormComponent {
         }
       };
       this.workoutService.addWorkout(workout);
+      
+      //msg on successful addition
+      this.snackBar.open('Workout added successfully!', 'Close', {
+        duration: 3000, 
+        horizontalPosition: 'center',
+        verticalPosition: 'top'
+      });
       this.workoutForm.reset();
+    }
+    else{
+      alert('Please fill out the form before submitting!');
     }
   }
 }
